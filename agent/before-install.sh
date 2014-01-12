4fdef1dffc29acbf2d92355b17ce9fa7f9567bce
Tom Akehurst <t.m.akehurst@googlemail.com>
2014-01-12 10:12:14 +0000

    Fixed startup and sudoers problems

--- agent/before-install.sh
+++ agent/before-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-sudo id -u breakbox &> /dev/null || sudo useradd -m -s /bin/false breakbox
+sudo id -u breakbox &> /dev/null || sudo useradd -m -s /bin/bash breakbox
 mkdir -p /var/run/breakbox
 chown breakbox:breakbox /var/run/breakbox
