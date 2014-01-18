04b658328b3c6b2661bd5834abb4e5bf3aec872e
vagrant <vagrant@node1.(none)>
2014-01-18 16:15:56 +0000

    Added log dir to before-install

--- agent/before-install.sh
+++ agent/before-install.sh
@@ -5,3 +5,6 @@
 sudo id -u breakbox &> /dev/null || sudo useradd -m -s /bin/bash breakbox
 mkdir -p /var/run/breakbox
 chown breakbox:breakbox /var/run/breakbox
+
+mkdir -p /var/log/breakbox
+chown breakbox:breakbox /var/log/breakbox
